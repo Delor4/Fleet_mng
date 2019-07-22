146155751e35968180c3e3250e46c66d71585309
Delor Rotstrump <delor4@gmail.com>
2019-07-22 17:17:49 +0200

    route vehicles/, route /vehicle/<int>/

--- fleetmngapp/urls.py
+++ fleetmngapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('vehicles/', views.vehicles, name='vehicles'),
+    path('vehicle/<int:vehicle_id>/', views.vehicle, name='vehicle')
 ]
--- fleetmngapp/views.py
+++ fleetmngapp/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from fleetmngapp.models import Vehicle
+
 
 def index(request):
     return HttpResponse("You're at the app index.")
+
+
+def vehicles(request):
+    return HttpResponse("Vehicles:<br>{}".format('<br>'.join(list(map(lambda x: str(x), Vehicle.objects.all())))))
+
+
+def vehicle(request, vehicle_id):
+    return HttpResponse("Vehicle {}".format(Vehicle.objects.get(id=vehicle_id)))

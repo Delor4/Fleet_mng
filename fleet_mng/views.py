af2daa84cba9d6d47680953ddf3b6420e4364814
Delor Rotstrump <delor4@gmail.com>
2019-07-22 19:17:16 +0200

    vehicles template

--- fleet_mng/views.py
+++ fleet_mng/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
 
 from fleet_mng.models import Vehicle
 
@@ -10,7 +11,12 @@ def index(request):
 
 
 def vehicles(request):
-    return HttpResponse("Vehicles:<br>{}".format('<br>'.join(list(map(lambda x: str(x), Vehicle.objects.all())))))
+    vehicles_list = Vehicle.objects.all()
+    template = loader.get_template('fleet_mng/vehicles.html')
+    context = {
+        'vehicles_list': vehicles_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def vehicle(request, vehicle_id):

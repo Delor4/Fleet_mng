e5f223571d2e0c61bfc6c19eb89e7d0a4bfcc470
Delor Rotstrump <delor4@gmail.com>
2019-07-22 16:09:11 +0200

    models.Vehicle.is_rented()

--- fleetmngapp/models.py
+++ fleetmngapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Vehicle(models.Model):
@@ -15,6 +16,10 @@ class Vehicle(models.Model):
 
     def __str__(self):
         return "{0} [{1} {2}]".format(self.name, self.brand, self.model)
+
+    def is_rented(self):
+        return Rent.objects.filter(vehicle=self, to_date__gte=timezone.now()).exists() or Rent.objects.filter(
+            vehicle=self, from_date__gte=timezone.now()).exists()
 
 
 class Renter(models.Model):

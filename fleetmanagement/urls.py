05bc2cb94188b9ee8f3ec1161a05ada20c52c47b
Delor Rotstrump <delor4@gmail.com>
2019-07-24 23:08:42 +0200

    auth starts

--- fleetmanagement/urls.py
+++ fleetmanagement/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('fleet_mng.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]

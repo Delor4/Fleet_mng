de682a5a1e59692ea283d6fa9bab171a0842ec9a
Delor Rotstrump <delor4@gmail.com>
2019-07-26 21:46:09 +0200

    permissions ...

--- fleet_mng/views_renter.py
+++ fleet_mng/views_renter.py
@@ -1,15 +1,18 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.views import generic
 
 from fleet_mng.models import Renter
 
 
-class RentersView(generic.ListView):
+class RentersView(PermissionRequiredMixin, generic.ListView):
+    permission_required = 'fleet_mng.view_renter'
     template_name = 'fleet_mng/renters.html'
 
     def get_queryset(self):
         return Renter.objects.all()
 
 
-class RenterView(generic.DetailView):
+class RenterView(PermissionRequiredMixin, generic.DetailView):
+    permission_required = 'fleet_mng.view_renter'
     model = Renter
     template_name = 'fleet_mng/renter.html'
--- fleet_mng/views_vehicle.py
+++ fleet_mng/views_vehicle.py
@@ -1,15 +1,18 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.views import generic
 
 from fleet_mng.models import Vehicle
 
 
-class VehiclesView(generic.ListView):
+class VehiclesView(PermissionRequiredMixin, generic.ListView):
+    permission_required = 'fleet_mng.view_vehicle'
     template_name = 'fleet_mng/vehicles.html'
 
     def get_queryset(self):
         return Vehicle.objects.all()
 
 
-class VehicleView(generic.DetailView):
+class VehicleView(PermissionRequiredMixin, generic.DetailView):
+    permission_required = 'fleet_mng.view_vehicle'
     model = Vehicle
     template_name = 'fleet_mng/vehicle.html'

b13e13de29ff0488180e93493e7ec5b061d19d4e
Delor Rotstrump <delor4@gmail.com>
2019-07-27 13:57:49 +0200

    renters - new and update

--- fleet_mng/urls_renter.py
+++ fleet_mng/urls_renter.py
@@ -5,4 +5,6 @@ from . import views_renter
 urlpatterns = [
     path('', views_renter.RentersView.as_view(), name='renters'),
     path('<int:pk>/', views_renter.RenterView.as_view(), name='renter'),
+    path('new/', views_renter.renter_new, name='renter_new'),
+    path('<int:pk>/edit/', views_renter.RenterUpdateView.as_view(), name='renter_edit'),
 ]
--- fleet_mng/views_renter.py
+++ fleet_mng/views_renter.py
@@ -1,5 +1,10 @@
+from django import forms
+from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from django.views import generic
+from django.views.generic import UpdateView
 
 from fleet_mng.models import Renter
 
@@ -13,9 +18,44 @@ class RentersView(PermissionRequiredMixin, generic.ListView):
     def get_queryset(self):
         return Renter.objects.all()
 
+
 # widok dla pojedyńczego obiektu modelu Renter
 # /renter/<int:pk>
 class RenterView(PermissionRequiredMixin, generic.DetailView):
     permission_required = 'fleet_mng.view_renter'
     model = Renter
     template_name = 'fleet_mng/renter.html'
+
+
+class RenterForm(forms.ModelForm):
+    class Meta:
+        model = Renter
+        fields = ('name', 'description')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+@login_required
+@permission_required('fleet_mng.add_renter')
+def renter_new(request):
+    if request.method == "POST":
+        form = RenterForm(request.POST)
+        if form.is_valid():
+            renter = Renter(name=form.cleaned_data.get('name'),
+                            description=form.cleaned_data.get('description'),
+                            )
+            renter.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = RenterForm()
+
+    return render(request, 'fleet_mng/renter_new.html', {'form': form})
+
+
+class RenterUpdateView(PermissionRequiredMixin, UpdateView):
+    permission_required = 'fleet_mng.change_renter'
+    model = Renter
+    form_class = RenterForm
+    template_name = 'fleet_mng/renter_new.html'
+    success_url = '/renter/'

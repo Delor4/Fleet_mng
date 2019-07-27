2028248c617f55402e3164c689ec1469bfd49bf8
Delor Rotstrump <delor4@gmail.com>
2019-07-27 12:37:11 +0200

    vehicles - new and update

--- fleet_mng/urls_vehicle.py
+++ fleet_mng/urls_vehicle.py
@@ -5,4 +5,7 @@ from fleet_mng import views_vehicle
 urlpatterns = [
     path('', views_vehicle.VehiclesView.as_view(), name='vehicles'),
     path('<int:pk>/', views_vehicle.VehicleView.as_view(), name='vehicle'),
+    path('new/', views_vehicle.vehicle_new, name='vehicle_new'),
+    path('<int:pk>/edit/', views_vehicle.VehicleUpdateView.as_view(), name='vehicle_edit'),
+
 ]
--- fleet_mng/views_vehicle.py
+++ fleet_mng/views_vehicle.py
@@ -1,3 +1,9 @@
+from django import forms
+from django.contrib.auth.decorators import login_required, permission_required
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import UpdateView
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.views import generic
 
@@ -15,8 +21,38 @@ class VehiclesView(PermissionRequiredMixin, generic.ListView):
 
 
 # pokazanie pojedyńczego pojazdu
-#/ vehicle/<int:pk> =>  vehicle.html
+# / vehicle/<int:pk> =>  vehicle.html
 class VehicleView(PermissionRequiredMixin, generic.DetailView):
     permission_required = 'fleet_mng.view_vehicle'
     model = Vehicle
     template_name = 'fleet_mng/vehicle.html'
+
+
+class VehicleForm(forms.ModelForm):
+    class Meta:
+        model = Vehicle
+        fields = ('name', 'brand', 'model', 'generation', 'registration_number')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+@login_required
+@permission_required('fleet_mng.add_vehicle')
+def vehicle_new(request):
+    if request.method == "POST":
+        form = VehicleForm(request.POST)
+        if form.is_valid():
+            # TODO: save new vehicle
+            return HttpResponseRedirect('/')
+    else:
+        form = VehicleForm()
+
+    return render(request, 'fleet_mng/vehicle_new.html', {'form': form})
+
+
+class VehicleUpdateView(UpdateView):
+    model = Vehicle
+    form_class = VehicleForm
+    template_name = 'fleet_mng/vehicle_new.html'
+    success_url = '/vehicle/'

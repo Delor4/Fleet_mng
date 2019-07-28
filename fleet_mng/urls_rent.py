94dec01a1aad8ac494e856baf3f2176c595fb341
Delor Rotstrump <delor4@gmail.com>
2019-07-28 20:13:04 +0200

    rent edit - urls

--- fleet_mng/urls_rent.py
+++ fleet_mng/urls_rent.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views_rent.RentsView.as_view(), name='rents'),
     path('<int:pk>/', views_rent.RentView.as_view(), name='rent'),
     path('new/', views_rent.show_rent_form, name='rent_form'),
+    path('<int:pk>/edit', views_rent.RentView.as_view(), name='rent_edit'),
     path('bring_back/<int:pk>/', views_rent.rent_bring_back, name='rent_bring_back'),
 ]

3bec444e4633d1e3c8902bbe5161b070b32387eb
Krzychu <krzyjaskowski@gmail.com>
2019-08-09 22:48:19 +0200

    renters update

--- fleet_mng/views_rent.py
+++ fleet_mng/views_rent.py
@@ -61,7 +61,7 @@ class RentForm(forms.Form):
 
         renters = Renter.objects.exclude(deleted=1)
         self.fields['renter'].choices = [(0, '-- nowy --')]
-        self.fields['renter'].choices.extend([(x.id, ("* ", "")[x.rented_count() == 0] + str(x)) for x in renters])
+        self.fields['renter'].choices.extend([(x.id, str(x)) for x in renters if x.rented_count() == 0])
 
     def clean(self):
         cleaned_data = super(RentForm, self).clean()

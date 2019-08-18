a761d6f92ceff5e048ee3b6a2ecf04ed664aec3c
Delor Rotstrump <delor4@gmail.com>
2019-08-18 10:30:01 +0200

    js fix

--- fleet_mng/static/fleet_mng/form_helpers.js
+++ fleet_mng/static/fleet_mng/form_helpers.js
@@ -4,6 +4,6 @@ function _delete(e,a,msg) {
     else
         window.event.returnValue=false;
     if(window.confirm(msg))
-        document.forms.delete_form.confirm.value=1;
-        document.forms.delete_form.submit();
+        a.confirm.value=1;
+        a.submit();
 }
